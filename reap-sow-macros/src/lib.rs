e69db6e1b11d4d591249eb16e6d586db1f275c50
Mego <Mego@users.noreply.github.com>
2025-06-08 19:19:12 -0500

    work with arbitrary types

--- reap-sow-macros/src/lib.rs
+++ reap-sow-macros/src/lib.rs
@@ -5,15 +5,22 @@ use syn::{
     visit_mut::{VisitMut, visit_expr_mut},
 };
 
-struct Sow;
+#[derive(Default)]
+struct Sow {
+    pub exprs: Vec<Expr>,
+}
 
 impl VisitMut for Sow {
     fn visit_expr_mut(&mut self, node: &mut syn::Expr) {
         if let Expr::Call(expr) = &node {
-            let arg = expr.args.first().unwrap();
-            if let Expr::Path(p) = &*expr.func {
-                if p.path.is_ident("sow") {
-                    *node = parse_quote!({ field.push(#arg); #arg });
+            if expr.args.len() == 1 {
+                let arg = &expr.args[0];
+                if let Expr::Path(p) = &*expr.func {
+                    if p.path.is_ident("sow") {
+                        let n = syn::Index::from(self.exprs.len());
+                        self.exprs.push(arg.clone());
+                        *node = parse_quote!({ field.#n });
+                    }
                 }
             }
         }
@@ -26,12 +33,13 @@ impl VisitMut for Sow {
 pub fn reap(input: TokenStream) -> TokenStream {
     let mut ast: Expr = parse_macro_input!(input);
 
-    Sow.visit_expr_mut(&mut ast);
+    let mut sow = Sow::default();
+    sow.visit_expr_mut(&mut ast);
+    let exprs = sow.exprs;
 
     let expanded = quote! {
         {
-            use std::any::Any;
-            let mut field: Vec<u64> = Vec::new();
+            let field = ( #( #exprs ),* , );
             (#ast, field)
         }
     };
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use reap_sow_macros::reap;
 
 fn main() {
-    dbg!(reap!(sow(3) + 100));
+    dbg!(reap!(sow(3)));
     dbg!(reap!(sow(5) * sow(100)));
+    dbg!(reap!(sow([1, 2, 3])[sow(1)]));
+    dbg!(reap!(sow("foo").chars().nth(sow(1)).unwrap()));
 }
